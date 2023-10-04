5402590360f41476f0def2134a0a2fe51e573c21
Farhatnasim <farhatnasim39@gmail.com>
2023-10-05 01:07:31 +0530

    done

--- src/main/java/com/driver/controllers/PassengerController.java
+++ src/main/java/com/driver/controllers/PassengerController.java
@@ -1,48 +1,23 @@
-package com.driver.EntryDto;
+package com.driver.controllers;
 
-import com.driver.model.Station;
 
-import java.time.LocalTime;
-import java.util.List;
+import com.driver.model.Passenger;
+import com.driver.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class AddTrainEntryDto {
+@RestController
+@RequestMapping("passenger")
+public class PassengerController {
 
-    private List<Station> stationRoute; //The items in the list denote the  order in which the train will move
+    @Autowired
+    PassengerService passengerService;
 
-    private LocalTime departureTime;
-
-    private int noOfSeats;
-
-    public List<Station> getStationRoute() {
-        return stationRoute;
-    }
-
-    public AddTrainEntryDto(List<Station> stationRoute, LocalTime departureTime, int noOfSeats) {
-        this.stationRoute = stationRoute;
-        this.departureTime = departureTime;
-        this.noOfSeats = noOfSeats;
-    }
-
-    public AddTrainEntryDto() {
-    }
-
-    public void setStationRoute(List<Station> stationRoute) {
-        this.stationRoute = stationRoute;
-    }
-
-    public LocalTime getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(LocalTime departureTime) {
-        this.departureTime = departureTime;
-    }
-
-    public int getNoOfSeats() {
-        return noOfSeats;
-    }
-
-    public void setNoOfSeats(int noOfSeats) {
-        this.noOfSeats = noOfSeats;
+    @PostMapping("/create")
+    public Integer registerPassenger(@RequestBody Passenger passenger){
+        return passengerService.addPassenger(passenger);
     }
 }
